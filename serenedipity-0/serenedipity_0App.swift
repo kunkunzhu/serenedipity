b6b90af485dcf1e2889738b4307ed8f595b0257f
Kun Zhu <73452544+kunkunzhu@users.noreply.github.com>
2024-06-28 11:54:30 -0400

    Initial Commit

---
+++ serenedipity-0/serenedipity_0App.swift
@@ -0,0 +1,17 @@
+//
+//  serenedipity_0App.swift
+//  serenedipity-0
+//
+//  Created by Kun Zhu on 2024-06-28.
+//
+
+import SwiftUI
+
+@main
+struct serenedipity_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
